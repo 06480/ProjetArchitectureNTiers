f3e291ef56bdab2a5161016cda3972edbf315dac
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-02-17 13:49:12 +0100

    ajout du findUserById

--- back/app.js
+++ back/app.js
@@ -1,7 +1,38 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://paulmessiant:epsi2023@cluster0.d7h8lhq.mongodb.net/test',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const port = 3000;
+const userSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: String,
+  email: String,
+});
+const beerSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  brand: String,
+  volume: Number,
+});
+const brewerySchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: String,
+  adress: String,
+});
+const User = mongoose.model("users", userSchema);
+const Beer = mongoose.model("beers", beerSchema);
+const brewery = mongoose.model("breweries", brewerySchema);
+mongoose
+  .connect("mongodb+srv://paulmessiant:epsi2023@cluster0.d7h8lhq.mongodb.net/ubeer", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((r) => {
+    console.log("connection réussie");
+    app.get("/user/:userId", (req, res) => {
+      let id = req.params.userId;
+      User.findById(id).then((user) => res.json(user));
+    });
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+    console.log("Connexion à MongoDB échouée !");
+  });
