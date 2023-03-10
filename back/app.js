0ec8dc4bddb6cc4125142acc21d88672b2d345ee
JuuLed <julien.ledoux@epsi.fr>
2023-03-10 10:30:15 +0100

    crud breweries

--- back/app.js
+++ back/app.js
@@ -16,11 +16,11 @@ const beerSchema = new mongoose.Schema({
 const brewerySchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
-  adress: String,
+  address: String,
 });
 const User = mongoose.model("users", userSchema);
 const Beer = mongoose.model("beers", beerSchema);
-const brewery = mongoose.model("breweries", brewerySchema);
+const Brewery = mongoose.model("breweries", brewerySchema);
 mongoose
   .connect("mongodb+srv://paulmessiant:epsi2023@cluster0.d7h8lhq.mongodb.net/ubeer", { useNewUrlParser: true, useUnifiedTopology: true })
   .then((r) => {
@@ -56,6 +56,8 @@ mongoose
       User.findByIdAndUpdate(req.params.id, { name: req.params.name, email: req.params.email, password: req.params.password }, (err, user) => {});
       res.status(204).send();
     });
+
+
     app.get("/beers/:beerId", (req, res) => {
       let id = req.params.beerId;
       Beer.findById(id).then((beer) => res.json(beer));
@@ -81,6 +83,35 @@ mongoose
       Beer.findByIdAndUpdate(req.params.id, { brand: req.params.brand, volume: req.params.volume }, (err, beer) => {});
       res.status(204).send();
     });
+
+
+    app.get("/breweries/:breweryId", (req, res) => {
+      let id = req.params.breweryId;
+      Brewery.findById(id).then((brewery) => res.json(brewery));
+    });
+    app.post("/breweries/create/:name/:address", (req, res) => {
+      let brewery = new Brewery({
+        _id: mongoose.Types.ObjectId(),
+        name: req.params.name,
+        address: req.params.address,
+      });
+      brewery.save((err, brewery) => {
+        console.log(brewery);
+      });
+
+      res.json();
+    });
+    app.delete("/breweries/delete/:id", (req, res) => {
+      Brewery.findByIdAndDelete(req.params.id, (err, brewery) => {
+        console.log(err);
+      });
+    });
+    app.patch("/breweries/change/:id/:name/:address", (req, res) => {
+      Brewery.findByIdAndUpdate(req.params.id, { name: req.params.name, address: req.params.address }, (err, brewery) => {});
+      res.status(204).send();
+    });
+
+
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
     });
