79bcde939d47fff2bfc910b08bf94e836f048369
Paul <paul.messiant1@gmail.com>
2023-04-21 10:12:32 +0200

    createBeer

--- front/src/views/Beers.vue
+++ front/src/views/Beers.vue
@@ -1,24 +1,39 @@
 <template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form fast-fail @submit.prevent>
+      <v-text-field v-model="brand" label="Brand"></v-text-field>
+
+      <v-text-field v-model="description" label="Description"></v-text-field>
+      <v-text-field
+        v-model="volume"
+        label="Volume"
+        :rules="volumeRules"
+      ></v-text-field>
+
+      <v-btn @click="createBeer" type="submit" block class="mt-2">Submit</v-btn>
+    </v-form>
+  </v-sheet>
   <v-text-field
     label="entrer le nom d'une bière"
     v-model="saisieUtilisateurBierre"
     @input="construireListeBieresFiltre"
   ></v-text-field>
+
   <ul v-for="biere in listeBieresFiltre">
     <v-card>
       <v-card-title>
         {{ biere["brand"] }}
       </v-card-title>
       <v-card-item>
-      {{ biere["description"] }}
+        {{ biere["description"] }}
       </v-card-item>
-      <v-card-item>
-        volume: {{ biere["volume"] }} cl
-      </v-card-item>
+      <v-card-item> volume: {{ biere["volume"] }} cl </v-card-item>
     </v-card>
   </ul>
 </template>
 <script lang="ts">
+import { AUTH0_TOKEN } from "@auth0/auth0-vue/dist/typings/token";
+import { stringifyExpression } from "@vue/compiler-core";
 import axios from "axios";
 //import { useAuth0 } from '@auth0/auth0-vue';
 export default {
@@ -28,18 +43,49 @@ export default {
       listeBieres: [],
       listeBieresFiltre: [],
       saisieUtilisateurBierre: "",
+      brand: "",
+      description: "",
+      volume: "",
+      volumeRules: [
+        (value: string) => {
+          if (value.match(/\b[0-9]+\b/)) {
+            return true;
+          } else {
+            return "le volume doit être seulement un nombre";
+          }
+        },
+      ],
     };
   },
   created() {
     this.initalisation();
   },
   methods: {
-    construireListeBieresFiltre(){
-      this.listeBieresFiltre = this.listeBieres.filter(e => {
-        let brand:string = e["brand"];
-        return  brand.toLowerCase().startsWith(this.saisieUtilisateurBierre.toLowerCase());
+    construireListeBieresFiltre() {
+      this.listeBieresFiltre = this.listeBieres.filter((e) => {
+        let brand: string = e["brand"];
+        return brand
+          .toLowerCase()
+          .startsWith(this.saisieUtilisateurBierre.toLowerCase());
       });
+    },
+    async createBeer() {
+      let token = await this.$auth0.getAccessTokenSilently();
 
+      axios
+        .post("https://ubeer.onrender.com/beers/create", {
+          headers: {
+            authorization: "Bearer" + token,
+          },
+          data: {
+            brand: this.brand,
+            description: this.description,
+            volume: this.volume,
+          },
+        })
+        .then((r) => {
+          console.log("createbeer");
+        });
     },
     async initalisation() {
       let token = await this.$auth0.getAccessTokenSilently();
