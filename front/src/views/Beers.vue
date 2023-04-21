7b6173cec853e9e2a5a421b2839038f5f411f6a2
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-04-21 11:09:17 +0200

    dkrfuirjifurfurfrurnjrfrjrfepjre

--- front/src/views/Beers.vue
+++ front/src/views/Beers.vue
@@ -91,7 +91,7 @@ export default {
         })
         .then((reponse) => {
           console.log(reponse.data)
-          idBierre = reponse.data.filter((e: any) => e["brand"])[0];
+          idBierre = reponse.data.filter((e: any) => e["brand"] === this.brand)[0];
           console.log(idBierre);
         })
         .then(async (e) => {
