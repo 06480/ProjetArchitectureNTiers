1812bc5223d500113bfde3684d32a369c7661678
Paul <paul.messiant1@gmail.com>
2023-04-21 12:23:17 +0200

    ajout form breweries

--- front/src/views/Breweries.vue
+++ front/src/views/Breweries.vue
@@ -1,66 +1,106 @@
 <template>
-    <v-text-field
-      label="entrer le nom d'une brasserie"
-      v-model="saisieUtilisateurBreweries"
-      @input="construireListeBreweriesFiltre"
-    ></v-text-field>
-    <ul v-for="brewery in listeBreweriesFiltre">
-      <v-card>
-        <v-card-title>
-          {{ brewery["name"] }}
-        </v-card-title>
-        <v-card-item>
+  <v-sheet width="300" class="mx-auto">
+    <v-form fast-fail @submit.prevent>
+      <v-text-field v-model="name" label="Nom"></v-text-field>
+
+      <v-text-field v-model="address" label="Adresse"></v-text-field>
+
+      <v-btn id="btn" @click="createBrewery" type="submit" block class="mt-2"
+        >Submit</v-btn
+      >
+    </v-form>
+  </v-sheet>
+  <v-text-field
+    label="entrer le nom d'une brasserie"
+    v-model="saisieUtilisateurBreweries"
+    @input="construireListeBreweriesFiltre"
+  ></v-text-field>
+  <ul v-for="brewery in listeBreweriesFiltre">
+    <v-card>
+      <v-card-title>
+        {{ brewery["name"] }}
+      </v-card-title>
+      <v-card-item>
         {{ brewery["address"] }}
-        </v-card-item>
-      </v-card>
-    </ul>
-  </template>
-  <script lang="ts">
-  import axios from "axios";
-  //import { useAuth0 } from '@auth0/auth0-vue';
-  export default {
-    name: "profile-view",
-    data() {
-      return {
-        listeBreweries: [],
-        listeBreweriesFiltre: [],
-        saisieUtilisateurBreweries: "",
-      };
+      </v-card-item>
+    </v-card>
+  </ul>
+</template>
+<script lang="ts">
+import axios from "axios";
+//import { useAuth0 } from '@auth0/auth0-vue';
+export default {
+  name: "profile-view",
+  data() {
+    return {
+      listeBreweries: [],
+      listeBreweriesFiltre: [],
+      saisieUtilisateurBreweries: "",
+      name: "",
+      address: "",
+    };
+  },
+  created() {
+    this.initalisation();
+  },
+  methods: {
+    construireListeBreweriesFiltre() {
+      this.listeBreweriesFiltre = this.listeBreweries.filter((e) => {
+        let name: string = e["name"];
+        return name
+          .toLowerCase()
+          .startsWith(this.saisieUtilisateurBreweries.toLowerCase());
+      });
     },
-    created() {
-      this.initalisation();
-    },
-    methods: {
-      construireListeBreweriesFiltre(){
-        this.listeBreweriesFiltre = this.listeBreweries.filter(e => {
-          let name:string = e["name"];
-          return  name.toLowerCase().startsWith(this.saisieUtilisateurBreweries.toLowerCase());
-        });
-  
-      },
-      async initalisation() {
-        let token = await this.$auth0.getAccessTokenSilently();
-  
-        console.log(token);
-  
+    async createBrewery() {
+      let token = await this.$auth0.getAccessTokenSilently();
+
+      if (this.name !== "" && this.address !== "")
         axios
-          .get("https://ubeer.onrender.com/breweries", {
-            headers: {
-              authorization: "Bearer " + token,
+          .post(
+            "https://ubeer.onrender.com/breweries/create",
+            {
+              name: this.name,
+              address: this.address,
             },
-          })
-          .then((reponse) => {
-            console.log("oui");
-            this.listeBreweries = reponse.data;
-            this.listeBreweriesFiltre = this.listeBreweries;
+            {
+              headers: {
+                authorization: "Bearer " + token,
+              },
+            }
+          )
+          .then((r) => {
+            console.log("createbeer");
+            window.location.href = "https://ubeer12.netlify.app/#/beers";
           })
           .catch((error) => {
-            console.log("----------------------- erreur -----------------------");
+            console.log("-".repeat(50));
             console.log(error);
-            console.log("----------------------- erreur -----------------------");
+            console.log("-".repeat(50));
           });
-      },
     },
-  };
-  </script>
-  
+    async initalisation() {
+      let token = await this.$auth0.getAccessTokenSilently();
+
+      console.log(token);
+
+      axios
+        .get("https://ubeer.onrender.com/breweries", {
+          headers: {
+            authorization: "Bearer " + token,
+          },
+        })
+        .then((reponse) => {
+          console.log("oui");
+          this.listeBreweries = reponse.data;
+          this.listeBreweriesFiltre = this.listeBreweries;
+        })
+        .catch((error) => {
+          console.log("----------------------- erreur -----------------------");
+          console.log(error);
+          console.log("----------------------- erreur -----------------------");
+        });
+    },
+  },
+};
+</script>
