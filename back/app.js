49c3f9d6dca9da09e913474d1b5e5a6f7ab4e17e
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-04-21 13:31:09 +0200

    ijfjiejfefj

--- back/app.js
+++ back/app.js
@@ -13,6 +13,11 @@ const jwtCheck = auth({
   issuerBaseURL: "https://dev-s15hjodwpfsobczx.eu.auth0.com/",
   tokenSigningAlg: "RS256",
 });
+const basketSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  beerId: [mongoose.Schema.Types.ObjectId],
+  userId: mongoose.Schema.Types.ObjectId,
+});
 const userSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
@@ -33,6 +38,7 @@ const brewerySchema = new mongoose.Schema({
 const User = mongoose.model("users", userSchema);
 const Beer = mongoose.model("beers", beerSchema);
 const Brewery = mongoose.model("breweries", brewerySchema);
+const Basket = mongoose.model("basket", basketSchema);
 mongoose
   .connect("mongodb+srv://paulmessiant:epsi2023@cluster0.d7h8lhq.mongodb.net/ubeer", { useNewUrlParser: true, useUnifiedTopology: true })
   .then((r) => {
@@ -44,6 +50,36 @@ mongoose
     });
     app.get("/callback", (req, res) => {
       res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+    });
+    app.get("/baskets", (req, res) => {
+      Basket.find({}, (err, documents) => {
+        res.json(documents);
+      });
+    });
+    app.get("/baskets/:basketId", (req, res) => {
+      let id = req.params.userId;
+      User.findById(id).then((user) => res.json(user));
+    });
+    app.post("/baskets/create", jsonParser, (req, res) => {
+      let user = new User({
+        _id: mongoose.Types.ObjectId(),
+        beersId: req.body.beersId,
+        userId: req.body.userId,
+      });
+      user.save((err, basket) => {
+        console.log(basket);
+      });
+
+      res.json();
+    });
+    app.delete("/baskets/delete/:id", (req, res) => {
+      Basket.findByIdAndDelete(req.params.id, (err, user) => {
+        console.log(err);
+      });
+    });
+    app.patch("/baskets/change/:id", jsonParser, (req, res) => {
+      Basket.findByIdAndUpdate(req.params.id, { beersId: req.body.beersId, userId: req.body.userId }, (err, user) => {});
+      res.status(204).send();
     });
     app.get("/users", (req, res) => {
       User.find({}, (err, documents) => {
@@ -93,7 +129,7 @@ mongoose
         _id: mongoose.Types.ObjectId(),
         brand: req.body.brand,
         volume: req.body.volume,
-        description: req.body.description
+        description: req.body.description,
       });
       beer.save((err, beer) => {
         console.log(beer);
@@ -110,7 +146,7 @@ mongoose
       Beer.findByIdAndUpdate(req.params.id, { brand: req.body.brand, volume: req.body.volume }, (err, beer) => {});
       res.status(204).send();
     });
-    
+
     app.get("/breweries/:breweryId", (req, res) => {
       let id = req.params.breweryId;
       Brewery.findById(id).then((brewery) => res.json(brewery));
